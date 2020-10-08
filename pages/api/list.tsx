a3e64455432fa4b99870674e9afa9a33601df8ea
Ronqui <sandro.ronqui.com>
2020-10-08 09:13:12 -0300

    22

--- pages/api/list.tsx
+++ pages/api/list.tsx
@@ -8,7 +8,6 @@ export default async (request: NowRequest, response: NowResponse) =>
 
   // let list = [{ id: 1, email: 'sandro', date: '20-10-2020' }, { id: 2, email: 'ronqui', date: '20-10-2020' }];
 
-  let list = [];
   console.log('1');
   const db = await connectToDatabase(process.env.MONGODB_URI);
 
@@ -16,7 +15,7 @@ export default async (request: NowRequest, response: NowResponse) =>
   const collection = db.collection('subscribers');
 
   console.log('3');
-  // let list = await collection.find();
+  let list = await collection.find();
   // // let list = await collection.find({ subscribedAt:{ $gt: new Date('2020-10-09'), $lt: new Date('2020-10-09') } });
   console.log('4');
 
