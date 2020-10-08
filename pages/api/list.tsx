0886185cef09085a881657402f920c38c473c182
Ronqui <sandro.ronqui.com>
2020-10-08 09:32:42 -0300

    23

--- pages/api/list.tsx
+++ pages/api/list.tsx
@@ -15,7 +15,7 @@ export default async (request: NowRequest, response: NowResponse) =>
   const collection = db.collection('subscribers');
 
   console.log('3');
-  let list = await collection.find();
+  let list = await collection.find({});
   // // let list = await collection.find({ subscribedAt:{ $gt: new Date('2020-10-09'), $lt: new Date('2020-10-09') } });
   console.log('4');
 
