e79a19e6a1cba63bab7f68d7ee8f14c374c81e7e
Ronqui <sandro.ronqui.com>
2020-10-09 16:19:01 -0300

    41

--- pages/api/list.tsx
+++ pages/api/list.tsx
@@ -8,7 +8,7 @@ export default async (request: NowRequest, response: NowResponse) =>
 {
   const params = request.query;
 
-  const list = data.sort((a, b) => a.id - b.id);
+  // const list = data.sort((a, b) => a.id - b.id);
 
   const db = await connectToDatabase(process.env.MONGODB_URI);
   const collection = db.collection('data');
@@ -18,10 +18,10 @@ export default async (request: NowRequest, response: NowResponse) =>
   //   // { $group: { _id: "$addDate", temp_average: { $avg: "$temp" } } }
   // ]);
 
+  const list = collection.find();
+
   console.log('collection :1', collection);
   console.log('list :1', list);
-
-  // const list = await collection.find();
 
   // const list = await collection.aggregate([{ $group: { _id: "$addDate", tem_average: { $avg: "$temp" } } }])
 
