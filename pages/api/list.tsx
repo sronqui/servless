2dedac861258410fcb434cef36f627ca62b0ef7e
Ronqui <sandro.ronqui.com>
2020-11-13 13:53:27 -0300

    12

--- pages/api/list.tsx
+++ pages/api/list.tsx
@@ -8,13 +8,18 @@ export default async (request: NowRequest, response: NowResponse) =>
 {
   const params = request.query;
 
+  // const list = data.sort((a, b) => a.id - b.id);
+  // console.log(list);
+  // return response.json(list);
+
   const db = await connectToDatabase(process.env.MONGODB_URI);
   const collection = db.collection('data');
-  collection.find({ _id: 0 })
+
+  collection.find({}, { projection: { _id: 0 } })
     .toArray()
     .then(list =>
     {
       console.log("all documents", list)
       return response.json(list);
-    })
+    });
 }
